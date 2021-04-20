3f0180e7e29ad43984bb3de06d723fdd3b7e4214
Jrock30 <jrg8989@knou.ac.kr>
2021-04-20 20:53:59 +0900

    Eureka Discovery Server Setting

--- src/test/java/com/jrock/discoveryservice/DiscoveryserviceApplicationTests.java
+++ src/test/java/com/jrock/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jrock.ecommerce;
+package com.jrock.discoveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class DiscoveryserviceApplicationTests {
 
     @Test
     void contextLoads() {
